82ed4f1edea075d53a707bda08223039714136bc
AronWinski <al0043@mix.wvu.edu>
2021-10-18 22:31:13 -0400

    synced with Zach's version and updated player controller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour
 
     private Rigidbody rb;
     public float force = 7.0f;
+    public float turningForce = 5.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -20,18 +21,16 @@ public class PlayerController : MonoBehaviour
         //add a velocity in the forward direction in relation to the rotation of the car
         if (Input.GetButton("Fire3"))
         {
-            rb.AddForce(new Vector3(force, 0, 0));
+            rb.AddRelativeForce(new Vector3(-force, 0, 0));
         }
+        //get the direction vector and 
         //rotate car
-
-
-
-
+        rb.angularVelocity = new Vector3(0, turningForce * Input.GetAxis("Horizontal"), 0);
     }
 
     //TODO: clamps the speed of the car so it can't go too fast
 
-    void processInput()
+    void ProcessInput()
     {
         float mh = Input.GetAxis("Horizontal");
         float mv = Input.GetAxis("Vertical");
