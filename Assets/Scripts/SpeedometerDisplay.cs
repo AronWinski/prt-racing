1ec5c786efbcae3882eceaec19ea85d80813982a
zmh0007 <zmh0007@mix.wvu.edu>
2021-11-09 18:49:07 -0500

    Added Speedometer Back

--- Assets/Scripts/SpeedometerDisplay.cs
+++ Assets/Scripts/SpeedometerDisplay.cs
@@ -6,10 +6,20 @@ using UnityEngine.UI;
 public class SpeedometerDisplay : MonoBehaviour {
 
     private int speed;
+<<<<<<< Updated upstream
 
    public void updateSpeed(float s)
     {
         GetComponent<Text>().text = "SPEED: " + (int) s;
+=======
+    public Text speedText;
+
+    // Update is called once per frame
+    public void updateSpeed(float s) {
+
+        GetComponent<Text>().text = "SPEED: " + (int) s;
+        
+>>>>>>> Stashed changes
     }
 
 
